504e823501f6b4d132dc190fb7e1185ae668ae15
Mark West <markwes@microsoft.com>
2017-05-01 12:18:26 -0700

    The NewWay() does list all the domain groups and does test membership of a given domain group name, TheOldWay() is how many old apps did it and it is working on the server context but not the domain (that I'm using).

--- AspNetEchoUsername/Default.aspx.cs
+++ AspNetEchoUsername/Default.aspx.cs
@@ -10,24 +10,26 @@ namespace AspNetEchoUsername
 {
     public partial class _Default : Page
     {
+        private const string _DomainGroupName = "MSIAllUsers";
+
+        //*********************************************************************
+        ///
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        /// 
+        //*********************************************************************
+
         protected void Page_Load(object sender, EventArgs e)
         {
             // First find if user is logged in
             if (Context.User.Identity.IsAuthenticated)
             {
-                // Finds user name and says Hi
                 lblWelcome.Text = "Hi " + Context.User.Identity.Name;
 
-                var bb = Context.User.IsInRole("LOCAL");
-                var cc = Context.User.IsInRole("TheBobs");
-
-                lblGroups.Text = "You are a member of these groups: ";
-
-                foreach (var group in GetGroups())
-                {
-                    //    lbGroups.Items.Add(group as string);
-                    lblGroups.Text += group as string + "; ";
-                }
+                //TheOldWay();
+                TheNewWay();
             }
             else
             {
@@ -36,10 +38,96 @@ namespace AspNetEchoUsername
             }
         }
 
-        public ArrayList GetGroups()
+        //*********************************************************************
+        ///
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        /// 
+        //*********************************************************************
+
+        private void TheNewWay()
+        {
+            var nameParts = Context.User.Identity.Name.Split(new char[] { '\\' });
+
+            var prinContext = new System.DirectoryServices.AccountManagement.PrincipalContext(
+                System.DirectoryServices.AccountManagement.ContextType.Domain, nameParts[0]);
+
+            var user =
+                System.DirectoryServices.AccountManagement.UserPrincipal.FindByIdentity(prinContext, Context.User.Identity.Name);
+
+            var groupy =
+                System.DirectoryServices.AccountManagement.GroupPrincipal.FindByIdentity(prinContext, _DomainGroupName);
+
+            lblGroups.Text = "You are a member of these groups: ";
+
+            foreach (var group in GetGroupNamesNew(user))
+                lblGroups.Text += group as string + "; ";
+
+            if (user.IsMemberOf(groupy))
+                lblGroupMembership.Text = "You are a member of '" + _DomainGroupName + "'";
+            else
+                lblGroupMembership.Text = "You are a not member of '" + _DomainGroupName + "'";
+        }
+
+        //*********************************************************************
+        ///
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="userPrin"></param>
+        /// <returns></returns>
+        /// 
+        //*********************************************************************
+
+        public ArrayList GetGroupNamesNew(System.DirectoryServices.AccountManagement.UserPrincipal userPrin)
         {
             ArrayList groups = new ArrayList();
-            foreach (System.Security.Principal.IdentityReference group in
+            foreach (var group in userPrin.GetGroups())
+                groups.Add(group.Name);
+
+            return groups;
+        }
+
+        //*********************************************************************
+        ///
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        /// 
+        //*********************************************************************
+
+        private void TheOldWay()
+        {
+            lblGroups.Text = "You are a member of these groups: ";
+
+            foreach (var group in GetGroupNamesOld())
+                lblGroups.Text += group as string + "; ";
+
+            var isInLocalGroup = Context.User.IsInRole("LOCAL");    //*** works because server local
+
+            if (Context.User.IsInRole(_DomainGroupName))            //*** doesn't work because domain
+                lblGroupMembership.Text = "You are a member of '" + _DomainGroupName + "'";
+            else
+                lblGroupMembership.Text = "You are a not member of '" + _DomainGroupName + "'";
+
+        }
+
+        //*********************************************************************
+        ///
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        /// 
+        //*********************************************************************
+
+        public ArrayList GetGroupNamesOld()
+        {
+            ArrayList groups = new ArrayList();
+            foreach (var group in
                 System.Web.HttpContext.Current.Request.LogonUserIdentity.Groups)
             {
                 groups.Add(group.Translate(typeof
--- AspNetEchoUsername/Default.aspx.designer.cs
+++ AspNetEchoUsername/Default.aspx.designer.cs
@@ -22,6 +22,15 @@ namespace AspNetEchoUsername {
         protected global::System.Web.UI.WebControls.Label lblWelcome;
         
         /// <summary>
+        /// lblGroupMembership control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblGroupMembership;
+        
+        /// <summary>
         /// lblGroups control.
         /// </summary>
         /// <remarks>
